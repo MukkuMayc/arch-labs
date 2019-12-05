1465298bfe3684b20bebf8687fdddcdf9c09742d
MukkuMayc <st063775@student.spbu.ru>
2019-12-05 22:50:05 +0300

    Create simple matrix class

--- libs/include/matrix.hpp
+++ libs/include/matrix.hpp
@@ -5,6 +5,35 @@
 #ifndef ARCH_LABS_MATRIX_HPP
 #define ARCH_LABS_MATRIX_HPP
 
+#include <ostream>
 
+template<typename T>
+class Matrix {
+    T **data;
+    size_t width;
+    size_t height;
 
-#endif //ARCH_LABS_MATRIX_HPP
+public:
+    explicit Matrix(size_t, size_t);
+
+    explicit Matrix(size_t);
+
+    T *operator[](size_t);
+
+    [[nodiscard]] size_t getWidth() const;
+
+    [[nodiscard]] size_t getHeight() const;
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &out, Matrix<T> &matrix) {
+    for (int i = 0; i < matrix.getHeight(); ++i) {
+        for (int j = 0; j < matrix.getWidth(); ++j) {
+            out << matrix[i][j] << ' ';
+        }
+        out << std::endl;
+    }
+    return out;
+}
+
+#endif // ARCH_LABS_MATRIX_HPP
--- libs/src/matrix.cpp
+++ libs/src/matrix.cpp
@@ -1,3 +1,34 @@
 //
 // Created by sergo on 12/5/19.
 //
+
+#include "matrix.hpp"
+
+template<typename T>
+Matrix<T>::Matrix(size_t n, size_t m): height(n), width(m) {
+    size_t pointersSpaceSize = sizeof(T *) * n;
+    size_t elementsRowSize = sizeof(T) * m;
+    data = reinterpret_cast<T **>(new char[pointersSpaceSize + elementsRowSize * n]);
+    for (size_t i = 0; i < n; ++i) {
+        data[i] = reinterpret_cast<T *>(reinterpret_cast<char *>(data) +
+                                        pointersSpaceSize + i * m * sizeof(T));
+    }
+}
+
+template<typename T>
+Matrix<T>::Matrix(size_t n): Matrix(n, n) {}
+
+template<typename T>
+T *Matrix<T>::operator[](size_t n) {
+    return data[n];
+}
+
+template<typename T>
+[[nodiscard]] size_t Matrix<T>::getWidth() const {
+    return width;
+}
+
+template<typename T>
+[[nodiscard]] size_t Matrix<T>::getHeight() const {
+    return height;
+}
