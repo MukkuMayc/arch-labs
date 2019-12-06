dcea61b937d243a16ddc9db1bd2b5881710f9b54
MukkuMayc <st063775@student.spbu.ru>
2019-12-06 03:15:44 +0300

    Add task 1

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -1,7 +1,72 @@
 //
 // Created by sergo on 12/5/19.
 //
+#include <iostream>
+#include "matrix.hpp"
+#include <chrono>
+
+using namespace std;
+
+int maxFromMinsParallel(Matrix<int> &A) {
+    int size = A.getWidth();
+    int rowMin[size];
+    int i, j;
+    int max = 0;
+#pragma omp parallel for default(shared), private(i, j), reduction(max:max)
+    for (i = 0; i < size; ++i) {
+        int min = 1e8;
+#pragma omp parallel for reduction(min:min), private(j)
+        for (j = 1; j < size; ++j) {
+            if (A[i][j] < min) {
+                min = A[i][j];
+            }
+        }
+        if (min > max) {
+            max = min;
+        }
+    }
+    cout << "Max: " << max << endl;
+
+    return max;
+}
+
+int maxFromMinsOneThread(Matrix<int> &A) {
+    int size = A.getWidth();
+    int rowMin[size];
+    int i, j;
+    int max = 0;
+    for (i = 0; i < size; ++i) {
+        int min = 1e8;
+        for (j = 1; j < size; ++j) {
+            if (A[i][j] < min) {
+                min = A[i][j];
+            }
+        }
+        if (min > max) {
+            max = min;
+        }
+    }
+    cout << "Max: " << max << endl;
+
+    return max;
+}
+
+template <typename F>
+void printExecTime(F &function, Matrix<int>& A) {
+    auto start = chrono::high_resolution_clock::now();
+    function(A);
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>
+                            (end - start).count() / 1e9;
+    cout << "Elapsed time: " << duration << endl;
+
+}
 
 int main() {
-  return 0;
+    const int size = 20000;
+    Matrix<int> A(size);
+    printExecTime(randomizeMatrix, A);
+    printExecTime(maxFromMinsOneThread, A);
+    printExecTime(maxFromMinsParallel, A);
+    return 0;
 }
